f6569788d77bdaf428327deebcf210681e06ee5b
Noor Awied <nooorawied@gmail.com>
2022-04-23 23:53:49 +0300

    Completed assignment with failed test

--- index.js
+++ index.js
@@ -8,9 +8,50 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(arr) {
+  if (arr == undefined) return [];
+  else {
+    return arr.map((title) => {
+      return title
+        .toLowerCase()
+        .split(' ')
+        .map((word) => {
+          let newWord = '';
+          switch (word) {
+            case 'stoppropagation':
+              newWord = 'StopPropagation';
+              break;
+            case 'preventdefault?':
+              newWord = 'PreventDefault?';
+              break;
+            case 'oo':
+              newWord = 'OO';
+              break;
+            case 'api':
+              newWord = 'API';
+              break;
+            case 'nan':
+              newWord = 'NaN';
+              break;
+            case 'jsonp?':
+              newWord = 'JSONP?';
+              break;
+            default:
+              newWord = word.charAt(0).toUpperCase() + word.slice(1);
+              break;
+          }
+          return newWord;
+        })
+        .join(' ');
+    });
+  }
 }
+console.log('original');
+console.log(tutorials);
+console.log('modified');
+
+let modifiedArray = titleCased(tutorials);
+console.log(modifiedArray);
